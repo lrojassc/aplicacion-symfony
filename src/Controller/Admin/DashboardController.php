d2b3be5ec50954a9e7ae06d4740403e83256e7c7
liever.rojas <liever.rojas@bitsamericas.com>
2023-04-21 14:11:58 -0500

    agregar ruta

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Category;
+use App\Entity\Comment;
+use App\Entity\Post;
+
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardController extends AbstractDashboardController
+{
+    #[Route('/admin', name: 'admin')]
+    public function index(): Response
+    {
+      $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
+
+      return $this->redirect($adminUrlGenerator->setController(PostCrudController::class)->generateUrl());
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('Aplicación');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        // yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+      yield MenuItem::linkToCrud('Categorias', 'fas fa-list', Category::class);
+      yield MenuItem::linkToCrud('Publicaciones', 'fas fa-list', Post::class);
+      yield MenuItem::linkToCrud('COmentarios', 'fas fa-list', Comment::class);
+
+
+    }
+}
